84b14a3a767174d7adfeee0cb574359694dc9ef4
dev-fathur <rifkifathurrohman10@gmail.com>
2025-10-06 09:21:48 +0700

    develop: navbar, footer, components

---
+++ resources/views/layouts/guest/footer.blade.php
@@ -0,0 +1,73 @@
+<footer class="text-base md:text-xl py-12">
+  <div class="text-center mb-10">
+    <h2 class="text-[24px] md:text-[32px] font-bold text-[#C8A565]">
+      Visit Our Adventure Base
+    </h2>
+  </div>
+
+  <div class="px-12 mx-auto grid grid-cols-1 md:grid-cols-4 gap-16">
+
+    <!-- Logo & Description -->
+    <div>
+      <div class="flex flex-wrap items-center space-x-3 mb-4">        
+        <img 
+          src="{{ asset('media/general/logo.svg') }}" 
+          alt="Logo" 
+          class="w-30 h-30 object-cover"
+        />
+        <h2 class="text-2xl md:text-4xl font-bold text-[#C8A565]">
+          Buggy Bali <br>
+          Adventure
+        </h2>
+      </div>
+      <p>
+        Experience 7 km of adrenaline on Bali’s only private buggy track;
+        mud, rice fields, caves, jungle, and speed circuits. Ride solo or
+        tandem with safety guides, then refresh with a river bath in a
+        natural canyon.
+      </p>
+    </div>
+
+    <!-- Contact Us -->
+    <div>
+      <h3 class="text-2xl md:text-4xl font-semibold text-[#C8A565] mb-4">Contact Us</h3>
+      <ul class="space-y-3 font-bold">
+        <li class="flex flex-wrap md:flex-nowrap justify-between items-center space-x-2">
+          <span>📞</span>
+          <span>+6285337284090</span>
+        </li>
+        <li class="flex justify-between items-center space-x-2">
+          <span>💬</span>
+          <span>+6285337284090</span>
+        </li>
+        <li class="flex text-wrap justify-between items-center space-x-2">
+          <span>✉️</span>
+          <span class="break-all">buggybaliadventure@gmail.com</span>
+        </li>
+      </ul>
+    </div>
+
+    <!-- Follow Us -->
+    <div>
+      <h3 class="text-2xl md:text-4xl font-semibold text-[#C8A565] mb-4">Follow Us</h3>
+      <ul class="space-y-3 font-bold">
+        <li class="flex justify-between items-center space-x-2">
+          <span>📷</span>
+          <span>@buggybaliadventure</span>
+        </li>
+        <li class="flex justify-between items-center space-x-2">
+          <span>🎵</span>
+          <span>@buggybaliadventure</span>
+        </li>
+      </ul>
+    </div>
+
+    <!-- Our Location -->
+    <div>
+      <h3 class="text-2xl md:text-4xl font-semibold text-[#C8A565] mb-4">Our Location</h3>
+      <div class="w-full h-40 rounded-lg overflow-hidden">        
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d505148.5912005096!2d114.74074021073775!3d-8.453261306288473!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd141d3e8100fa1%3A0x24910fb14b24e690!2sBali!5e0!3m2!1sid!2sid!4v1759506293783!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </div>
+  </div>
+</footer>
--- routes/guest.php
+++ routes/guest.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
   return view('guest.home.index');
 });
+Route::get('/utv-packages', function () {
+  return view('guest.home.index');
+});
+Route::get('/activity-packages', function () {
+  return view('guest.home.index');
+});
+Route::get('/gallery', function () {
+  return view('guest.home.index');
+});
+Route::get('/aboutus', function () {
+  return view('guest.home.index');
+});
