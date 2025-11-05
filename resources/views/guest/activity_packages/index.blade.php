d4054c65b37e5659cf83ce83451353272160e0e7
RozKoy <okkoy.1401@gmail.com>
2025-11-05 22:32:43 +0700

    feat(all page): remove dummy data

--- resources/views/guest/activity_packages/index.blade.php
+++ resources/views/guest/activity_packages/index.blade.php
@@ -2,36 +2,36 @@
 
 @php
     $collections = collect([
-        [
-            'title' => 'Single ATV + Rafting',
-            'description' => 'Rafting — Ride solo on 1 ATV + 1 rafting pack (for 1 person)',
-            'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
-        ],
-        [
-            'title' => 'Tandem ATV + Rafting',
-            'description' => '2 people on 1 ATV + 2 rafting packs',
-            'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
-        ],
-        [
-            'title' => 'Single ATV 300 CC + Rafting',
-            'description' => 'Powerful solo ride (300cc) + 1 rafting pack',
-            'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
-        ],
-        [
-            'title' => 'Tandem ATV 300 CC + Rafting',
-            'description' => '300cc ride for 2 + 2 rafting packs',
-            'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
-        ],
-        [
-            'title' => 'Single ATV + Tubing',
-            'description' => '1 ATV for 1 person + 1 tubing pack',
-            'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
-        ],
-        [
-            'title' => 'Tandem ATV + Tubing',
-            'description' => '1 ATV for 2 people + 2 tubing packs',
-            'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
-        ],
+        // [
+        //     'title' => 'Single ATV + Rafting',
+        //     'description' => 'Rafting — Ride solo on 1 ATV + 1 rafting pack (for 1 person)',
+        //     'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
+        // ],
+        // [
+        //     'title' => 'Tandem ATV + Rafting',
+        //     'description' => '2 people on 1 ATV + 2 rafting packs',
+        //     'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
+        // ],
+        // [
+        //     'title' => 'Single ATV 300 CC + Rafting',
+        //     'description' => 'Powerful solo ride (300cc) + 1 rafting pack',
+        //     'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
+        // ],
+        // [
+        //     'title' => 'Tandem ATV 300 CC + Rafting',
+        //     'description' => '300cc ride for 2 + 2 rafting packs',
+        //     'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
+        // ],
+        // [
+        //     'title' => 'Single ATV + Tubing',
+        //     'description' => '1 ATV for 1 person + 1 tubing pack',
+        //     'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
+        // ],
+        // [
+        //     'title' => 'Tandem ATV + Tubing',
+        //     'description' => '1 ATV for 2 people + 2 tubing packs',
+        //     'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
+        // ],
     ]);
 @endphp
 
@@ -50,10 +50,15 @@
     </x-hero-image>
 
     <div class="w-full bg-white">
-        <div class="w-full max-w-screen-2xl mx-auto grid md:grid-cols-2 py-14 lg:py-20 justify-items-center">
-            @foreach ($collections as $key => $item)
-                <x-card-product type_card="{{ $key % 2 == 0 ? 'left' : 'right' }}" img="{{ $item['img'] }}" alt_img="Buggy Adventure" title="{{ $item['title'] }}" description="{{ $item['description'] }}" url="#" />
-            @endforeach
-        </div>
+        @if ($collections->count())
+            <div class="w-full max-w-screen-2xl mx-auto grid md:grid-cols-2 py-14 lg:py-20 justify-items-center">
+                @foreach ($collections as $key => $item)
+                    <x-card-product type_card="{{ $key % 2 == 0 ? 'left' : 'right' }}" img="{{ $item['img'] }}" alt_img="Buggy Adventure" title="{{ $item['title'] }}" description="{{ $item['description'] }}" url="#" />
+                @endforeach
+            </div>
+        @endif
+        @if (!$collections->count())
+            <p class="text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl text-center text-black w-full py-14 lg:py-20">NO DATA</p>
+        @endif
     </div>
 @endsection
--- resources/views/guest/galleries/index.blade.php
+++ resources/views/guest/galleries/index.blade.php
@@ -5,32 +5,22 @@
         [
             'title' => 'Single ATV + Rafting',
             'description' => 'Rafting — Ride solo on 1 ATV + 1 rafting pack (for 1 person)',
-            'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
+            'img' => '/media/element/about-us.png',
         ],
         [
             'title' => 'Tandem ATV + Rafting',
             'description' => '2 people on 1 ATV + 2 rafting packs',
-            'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
+            'img' => '/media/element/find-us.png',
         ],
         [
             'title' => 'Single ATV 300 CC + Rafting',
             'description' => 'Powerful solo ride (300cc) + 1 rafting pack',
-            'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
+            'img' => '/media/element/gallery.png',
         ],
         [
             'title' => 'Tandem ATV 300 CC + Rafting',
             'description' => '300cc ride for 2 + 2 rafting packs',
-            'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
-        ],
-        [
-            'title' => 'Single ATV + Tubing',
-            'description' => '1 ATV for 1 person + 1 tubing pack',
-            'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
-        ],
-        [
-            'title' => 'Tandem ATV + Tubing',
-            'description' => '1 ATV for 2 people + 2 tubing packs',
-            'img' => 'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg',
+            'img' => '/media/element/home.png',
         ],
     ]);
 @endphp
@@ -44,24 +34,23 @@
         </div>
     </x-hero-image>
 
-    <div class="w-full bg-white space-y-8 md:space-y-12 lg:space-y-16 xl:space-y-20 px-6">
-        <div class="w-full max-w-screen-2xl mx-auto flex flex-col gap-8 text-black pt-14 lg:pt-20">
+    <div class="w-full bg-white space-y-8 md:space-y-12 lg:space-y-16 xl:space-y-20 px-6" x-data="{
+        preview: '{{ $collections->first()['img'] }}'
+    }">
+        <div id="preview" class="w-full max-w-screen-2xl mx-auto flex flex-col gap-8 text-black pt-14 lg:pt-20">
             <h3 class="text-xl md:text-2xl lg:text-3xl font-bold text-center">
                 CAPTURED THRILLS
             </h3>
             <p class="text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl font-bold text-center">
                 Discover the highlights of our buggy adventure through photos.
             </p>
-            <img src="https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/10/3e/2c/14.jpg" alt="" srcset="" class="mx-auto w-11/12 md:w-8/12">
+            <img x-bind:src="preview" alt="" srcset="" class="mx-auto w-11/12 md:w-8/12">
         </div>
         <div class="w-full max-w-screen-2xl mx-auto flex flex-wrap justify-center items-center gap-5 pb-14 lg:pb-20">
-            @for ($i = 0; $i < 5; $i++)
-                @foreach ($collections as $key => $item)
-                    <div class="w-80 aspect-video overflow-hidden" title="{{ $item['title'] }}">
-                        <img src="{{ $item['img'] }}" alt="" srcset="" class="object-cover">
-                    </div>
-                @endforeach
-            @endfor
+            @foreach ($collections as $key => $item)
+                <a href="#preview" class="w-80 aspect-video bg-no-repeat bg-cover bg-center" style="background-image: url('{{ $item['img'] }}')" title="{{ $item['title'] }}" x-on:click="preview = '{{ $item['img'] }}'">
+                </a>
+            @endforeach
         </div>
     </div>
 @endsection
